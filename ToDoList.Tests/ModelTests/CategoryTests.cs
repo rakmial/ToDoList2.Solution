efe20b240d2460aacfe1c504d5a84fbe7947774d
Joshua Lovelace <rakmial@gmail.com>
2020-09-24 14:43:13 -0700

    add intertest _instances disposal

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -6,13 +6,28 @@ using System;
 namespace ToDoList.Test
 {
   [TestClass]
-  public class CategoryTest
+  public class CategoryTest : IDisposable
   {
+
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
+
     [TestMethod]
     public void CategoryConstructor_CreatesCategoryInstance_Category()
     {
       Category newCat = new Category("test");
       Assert.AreEqual(typeof(Category), newCat.GetType());
+    }
+
+    [TestMethod]
+    public void IdGetter_ReturnsIdOfCategory_Int()
+    {
+      Category test1 = new Category("test1");
+      Category test2 = new Category("test2");
+      Assert.AreEqual(test1.Id, 1);
+      Assert.AreEqual(test2.Id, 2);
     }
   }
 }
