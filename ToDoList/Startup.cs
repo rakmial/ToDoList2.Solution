331d9a194db038c63e8521b2654c45d2eb64698d
Joshua Lovelace <rakmial@gmail.com>
2020-09-29 11:19:55 -0700

    add MySQL config

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -43,4 +43,13 @@ namespace ToDoList
 
     }
   }
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "\
+    server=localhost;\
+    user id=root;\
+    password=crabcake;\
+    port=3306;\
+    database=to_do_list;"
+  }
 }
